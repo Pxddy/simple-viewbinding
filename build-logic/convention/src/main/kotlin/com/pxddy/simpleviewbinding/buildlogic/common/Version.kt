9330cc87db6657e9c8a61c6baacf2c07309c1ff6
Pxddy <patrick.henke@t-online.de>
2025-11-22 17:43:23 +0100

    Update minSdk, target and compile sdk

--- build-logic/convention/src/main/kotlin/com/pxddy/simpleviewbinding/buildlogic/common/Version.kt
+++ build-logic/convention/src/main/kotlin/com/pxddy/simpleviewbinding/buildlogic/common/Version.kt
@@ -16,9 +16,9 @@ object Version {
     }
 
     object Sdk {
-        const val min = 21
-        const val target = 35
-        const val compile = 35
+        const val min = 23
+        const val target = 36
+        const val compile = 36
     }
 
     object Java {
